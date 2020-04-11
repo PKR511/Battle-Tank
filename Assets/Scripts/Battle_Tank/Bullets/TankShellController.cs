20f2c7b8c05fbf2302aa8af3a223581bfd4b13fc
PKR511 <57657443+PKR511@users.noreply.github.com>
2020-04-11 12:26:31 +0530

    feature2-MVC
    Tank Movement And Fire Bullet

---
+++ Assets/Scripts/Battle_Tank/Bullets/TankShellController.cs
@@ -0,0 +1,35 @@
+﻿using Battle_Tank.Bullets;
+using Battle_Tank.Bullets;
+using UnityEngine;
+
+namespace Battle_Tank.Bullets{
+public class TankShellController  {
+
+	
+		//private variables
+		private TankShellModel tankShellModel;
+		private TankShellView tankShellView;
+
+		//Property
+		public  TankShellModel TankShellModel {get{return tankShellModel;}}
+		public  TankShellView TankShellView {get{return tankShellView;}}
+		//Default Constructor
+		public TankShellController(TankShellModel shellModel, TankShellView shellView,GameObject parent,Vector3 pos,Quaternion rot){
+			this.tankShellModel = shellModel;
+			this.tankShellView = GameObject.Instantiate<TankShellView>(shellView);
+			this.tankShellView.transform.SetParent (parent.transform);
+			this.tankShellView.transform.position = pos;
+			this.tankShellView.transform.rotation = rot;
+			this.tankShellView.Initialize (shellController: this);
+			this.tankShellView.Move ();
+			Debug.Log ("Tank instantiated");
+
+		}//TankController
+
+
+
+
+
+
+}//Class
+}//namespace
---
+++ Assets/Scripts/Battle_Tank/Bullets/TankShellModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Battle_Tank.Bullets{
+public class TankShellModel {
+
+		//private Variables
+		private float speed;
+		private bool active, spawned;
+		//Property
+		public  float Speed {get{return speed;}set{speed=value;}}
+		public  bool Active {get{return active;}set{active=value;}}
+		public  bool Spawned {get{return spawned;}set{spawned=value;}}
+
+
+		//Default Constructor
+		public TankShellModel(float speed){
+
+			this.speed = speed;
+			this.active = true;
+			this.spawned = true;
+
+		}//TankShellModel
+
+
+	
+}//Class
+}//Namespace
